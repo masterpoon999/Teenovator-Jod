43841210eaf2715deb4e66b45ab6d49f9d83eaad
Your Name <your@email.com>
2025-08-19 10:23:56 +0700

    update App.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,3 @@
-// src/firebase.js
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
@@ -7,7 +6,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyBF5Fvt3lScB1Pzo_fmeYBlGIPAtW6FU58",
   authDomain: "teenovator-jod.firebaseapp.com",
   projectId: "teenovator-jod",
-  storageBucket: "teenovator-jod.appspot.com",   // ✅ แก้ตรงนี้
+  storageBucket: "teenovator-jod.appspot.com",
   messagingSenderId: "517161494043",
   appId: "1:517161494043:web:e375f74a48c7e2fa54fabe"
 };
@@ -17,4 +16,4 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 
-export default app;   // ✅ เพิ่ม export เผื่อบางไฟล์ต้องใช้
+export default app; 
